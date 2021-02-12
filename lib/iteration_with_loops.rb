f3bced569f14d0e5d38332a527359e7c103d6bd3
Nicholas Frangione <nfrangione8@gmail.com>
2021-02-12 05:20:06 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,5 +6,5 @@ def join_nested_strings(src)
   while count < src.count do
     inner_count = 0 
     while inner_count < src[count].count do
-      if src[count][inner_count].to_s == 
+      if src[count][inner_count]
 end
